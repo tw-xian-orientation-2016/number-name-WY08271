25e2ae7d1b0e6ed93fa375f8bb6d1046fff7c844
WY08271 <wy08271@outlook.com>
2016-03-01 16:48:45 +0800

    add main.js file and splitNumber() function

---
+++ main/main.js
@@ -0,0 +1,10 @@
+function splitNumber(number){
+    var upsideNumber = number.split("").reverse().join("");
+    var threeDigits = upsideNumber.match(/.{3}|.+$/g);
+
+    for (var i = 0; i < threeDigits.length; i++){
+        threeDigits[i] = threeDigits[i].split("").reverse().join("");
+    }
+
+    return threeDigits;
+}
