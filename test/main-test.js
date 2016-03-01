ab70fb67c6be49edcef62f853974b041be6c571d
WY08271 <wy08271@outlook.com>
2016-03-01 18:17:18 +0800

    format splitNumber test file

--- test/main-test.js
+++ test/main-test.js
@@ -24,18 +24,18 @@ describe('#splitNumber()', function () {
             expect(expectResult).toEqual(result);
         })
 
-        it('can split number length is 6',function(){
+        it('can split number length is 6', function () {
             var number = '234567';
             var result = splitNumber(number);
-            var expectResult = ['567','234'];
+            var expectResult = ['567', '234'];
 
             expect(expectResult).toEqual(result);
         })
 
-        it('can split number length is 7',function(){
+        it('can split number length is 7', function () {
             var number = '2345678';
             var result = splitNumber(number);
-            var expectResult = ['678','345','2'];
+            var expectResult = ['678', '345', '2'];
 
             expect(expectResult).toEqual(result);
         })
