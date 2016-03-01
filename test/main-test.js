be5ec3176a186dd6bea59f51318f4b955459a9ac
WY08271 <wy08271@outlook.com>
2016-03-01 21:08:00 +0800

    add translateThree() test

--- test/main-test.js
+++ test/main-test.js
@@ -79,4 +79,70 @@ describe('#translateThreeDigits()', function () {
             expect(result).toEqual(expectResult);
         })
     })
+
+    describe('.getThreeDigit()', function () {
+        it('can translage three digits, number is 111', function () {
+            var number = '111';
+            var result = translateThree(number);
+            var expectResult = 'one hundred and eleven';
+
+            expect(result).toEqual(expectResult);
+        })
+
+        it('can translage three digits, number is 011', function () {
+            var number = '011';
+            var result = translateThree(number);
+            var expectResult = 'eleven';
+
+            expect(result).toEqual(expectResult);
+        })
+
+        it('can translage three digits, number is 110', function () {
+            var number = '110';
+            var result = translateThree(number);
+            var expectResult = 'one hundred and ten';
+
+            expect(result).toEqual(expectResult);
+        })
+
+        it('can translage three digits, number is 010', function () {
+            var number = '010';
+            var result = translateThree(number);
+            var expectResult = 'ten';
+
+            expect(result).toEqual(expectResult);
+        })
+
+        it('can translage three digits, number is 101', function () {
+            var number = '101';
+            var result = translateThree(number);
+            var expectResult = 'one hundred and one';
+
+            expect(result).toEqual(expectResult);
+        })
+
+        it('can translage three digits, number is 001', function () {
+            var number = '001';
+            var result = translateThree(number);
+            var expectResult = 'one';
+
+            expect(result).toEqual(expectResult);
+        })
+
+        it('can translage three digits, number is 100', function () {
+            var number = '100';
+            var result = translateThree(number);
+            var expectResult = 'one hundred';
+
+            expect(result).toEqual(expectResult);
+        })
+
+        it('can translage three digits, number is 000', function () {
+            var number = '000';
+            var result = translateThree(number);
+            var expectResult = '';
+
+            expect(result).toEqual(expectResult);
+        })
+    })
 })
